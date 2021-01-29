e2d2bce78d46893746e23f2acd3afa7dd90e64af
Nasser BOUKEHIL <boukehil.nasreddine@gmail.com>
2021-01-29 17:59:01 +0100

    added observer ecnt calls example

---
+++ src/main/java/rx/demo/RxJavaCreateDemo.java
@@ -0,0 +1,30 @@
+package rx.demo;
+
+import io.reactivex.Observable;
+
+public class RxJavaCreateDemo {
+
+	public static void main(String[] args) {
+		Observable<String> daysOfWeek = Observable.create(
+										sourceEmitter -> {
+											try {
+												sourceEmitter.onNext("Sunday");
+												sourceEmitter.onNext("Monday");
+												sourceEmitter.onNext("Tuesday");
+												sourceEmitter.onNext("Wednesday");
+												sourceEmitter.onNext("Thursday");
+												sourceEmitter.onNext("Friday");
+												sourceEmitter.onNext("Saturday");
+												sourceEmitter.onComplete();
+												sourceEmitter.onNext("Will not be transmetted");
+											} catch (Exception e) {
+												sourceEmitter.onError(e);
+											}
+										});
+		
+		Observable<String> daysInUpperCase = daysOfWeek.map(day -> day.toUpperCase())
+														.filter(day -> day.startsWith("S"));
+		
+		daysInUpperCase.subscribe(day -> System.out.println("Day is ==> : " + day));
+	}
+}
